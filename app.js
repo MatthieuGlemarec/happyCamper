549ecbec5e467904f6d127c88870610e71654327
Matthieu <matthieuglemarec@gmail.com>
2023-10-24 10:49:29 +0100

    clean up

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 const dbUrl = 'mongodb://127.0.0.1:27017/happy-camper'
+
+
 // process.env.DB_URL
 
 
